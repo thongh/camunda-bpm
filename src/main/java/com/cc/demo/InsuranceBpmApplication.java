ff15387845c8935bf0c26411e62faf4bac4aa466
Thong Huynh <thongh@coutureconsulting.com>
2018-09-07 16:51:27 +0700

    first commit

---
+++ src/main/java/com/cc/demo/InsuranceBpmApplication.java
@@ -0,0 +1,12 @@
+package com.cc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceBpmApplication.class, args);
+	}
+}
